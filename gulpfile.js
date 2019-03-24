4b7cf8bf88a8a465562de348c812260c7491ebda
Janiel Martell <martell.janiel@gmail.com>
2019-03-24 01:04:36 -0400

    add production css processing

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,71 @@ const {
     series
 } = require('gulp');
 
-const postcss = require('gulp-postcss'); // CSS preprocessor
 const tailwindcss = require('tailwindcss');
+const postcss = require('gulp-postcss'); // CSS preprocessor
 
-// Plugins
+// PostCSS Plugins
+const purgecss = require('@fullhuman/postcss-purgecss'); // removes unused classes
 const autoprefixer = require('autoprefixer'); // adds vendor prefixes to CSS rules using https://caniuse.com
+const cssnano = require('cssnano'); // minifies css
 
+const path = {
+    html: './index.html',
+    css: './src/styles.css',
+    config: './src/tailwind.config.js',
+    dev: './resources/css/',
+    prod: './dist/css/'
+};
+
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ */
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-z0-9-:\/]+/g);
+    }
+}
 
 function buildDev() {
     let plugins = [
-        tailwindcss('./src/tailwind.config.js'),
+        tailwindcss(path.config),
         autoprefixer
     ];
-    return src('./src/styles.css')
+    return src(path.css)
         .pipe(postcss(plugins))
-        .pipe(dest('./resources/css/'));
+        .pipe(dest(path.dev));
 }
 
 
 function watchTailwind() {
-    watch('./src/styles.css', buildDev);
-    watch('./src/tailwind.config.js', buildDev);
+    watch(path.css, buildDev);
+    watch(path.config, buildDev);
+}
+
+function build() {
+    let cssnanoConfig = {
+        preset: 'default'
+    };
+
+    let purgecssConfig = {
+        content: [path.html],
+        extractors: [{
+            extractor: TailwindExtractor,
+            extensions: ["html", "js"]
+        }]
+    };
+    let plugins = [
+        tailwindcss(path.config),
+        purgecss(purgecssConfig),
+        autoprefixer,
+        cssnano(cssnanoConfig)
+    ];
+
+    return src(path.css)
+        .pipe(postcss(plugins))
+        .pipe(dest(path.prod));
 }
 
 exports.default = series(buildDev, watchTailwind);
+exports.build = build;
